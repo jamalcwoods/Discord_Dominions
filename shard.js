cf540cee085f9134955b69677306d1a61c415200
Jamal Woods <JamalCWoods@Gmail.com>
2021-05-06 13:20:27 -0400

    hidding pubnub credentials

--- shard.js
+++ shard.js
@@ -3,12 +3,9 @@ fs = require('fs'),
 request = require('request'),
 PubNub = require('pubnub'),
 terminator = require('terminate'),
+const credentials = require("./credentials.json"),
 colors = require('colors'),
-pubnub = new PubNub({
-    publishKey: 'pub-c-2611a1e2-dc0e-4f70-be94-131b81e4b33d',
-    subscribeKey: 'sub-c-3b821aee-101c-11e8-bb6e-d6d19ee12a32',
-    ssl: true
-}),
+pubnub = new PubNub(credentials.pubnub),
 shards = [],
 numberShards = 1;
 var manager = new Discord.ShardingManager('./index.js', { respawn: false });
